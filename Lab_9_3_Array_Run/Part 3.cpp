f5965599fef3c744fdf2ae1a12c600306891c780
m-seveigny <m-seveigny@onu.edu>
2025-10-30 14:21:03 -0400

    Completed P9-3

--- Lab_9_3_Array_Run/Part 3.cpp
+++ Lab_9_3_Array_Run/Part 3.cpp
@@ -13,6 +13,9 @@ void displayRun( int values[], int size )
 This function receives an array of die values and prints out the values in order, encapsulating the runs within
 parentheses such that there is no space between the left parenthesis and the first value within a run, and no
 space between the last value within a run and the right parenthesis.
+Once displayRun is complete, implement hasRun:
+bool hasRun( int values[], int size )
+This function returns true if a run is found within the die array. It does NOT print anything out.
 
 */
 #include <iostream>
@@ -20,8 +23,12 @@ space between the last value within a run and the right parenthesis.
 #include <ctime>
 using namespace std;
 
+// Constants
 const int sizeMax = 20;
+
+// Function prototypes
 void displayRun(int values[], int size);
+bool hasRun(int values[], int size);
 
 int main(void) {
 	// Declares variables
@@ -31,15 +38,15 @@ int main(void) {
 	// Seed random number generator
 	srand(unsigned(time(0)));
 
-	// Creat array (random values) and display without runs
-	cout << "No run: ";
+	// Create array (random values)
 	for (int i = 0; i < dieValues; i++) {
 		values[i] = (rand() % 6) + 1; // Random number between 1 and 6
-		cout << values[i] << " ";
 	}
-	cout << endl;
 
 	// Display array with runs
+	if (!hasRun(values, dieValues))
+		cout << "No run: ";
+	else
 	cout << "Has run: ";
 	displayRun(values, dieValues);
 	return 0;
@@ -64,4 +71,12 @@ void displayRun(int values[], int size) {
 		}
 	}
 	cout << endl;
+}
+bool hasRun(int values[], int size) {
+	for (int i = 0; i < size - 1; i++) {
+		if (values[i] == values[i + 1]) {
+			return true;
+		}
+	}
+	return false;
 }
